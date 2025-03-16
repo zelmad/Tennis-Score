fbc938ad19b8ecd0aad01c65d4f217ef4ca5ac3c
zelmadmohamed1994@gmail.com <Lhrto1994@>
2025-03-16 17:59:51 +0200

    Add unit tests before implementing functionality

--- src/main/java/org/tennis/TennisScore.java
+++ src/main/java/org/tennis/TennisScore.java
@@ -2,11 +2,11 @@ package org.tennis;
 
 public class TennisScore {
 
-    public String getWinner(String input) {
+    public String getWinner(String input) throws InvalidInputException {
         return null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidInputException {
         TennisScore tennisScore = new TennisScore();
         System.out.println(tennisScore.getWinner("ABABABABAAAA"));
     }
--- src/test/java/TennisScoreTest.java
+++ src/test/java/TennisScoreTest.java
@@ -1,5 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.tennis.InvalidInputException;
 import org.tennis.TennisScore;
+
+import static org.junit.Assert.assertEquals;
 
 public class TennisScoreTest {
 
@@ -8,5 +13,42 @@ public class TennisScoreTest {
     @BeforeEach
     void setUp() {
         tennisScore = new TennisScore();
+    }
+
+    @Test
+    public void test_player_invalid_input_exception() {
+        try{
+            String winnerPlayer = tennisScore.getWinner("QBABAA");
+        } catch (InvalidInputException e) {
+            String expectedWinner = "Invalid character: Q Is should be A or B";
+            assertEquals(e.getMessage(), expectedWinner);
+        }
+    }
+    @Test
+    public void test_player_a_win() throws InvalidInputException {
+        String winnerPlayer = tennisScore.getWinner("ABABAA");
+        String expectedWinner = "Player A wins the game";
+        assertEquals(winnerPlayer, expectedWinner);
+    }
+
+    @Test
+    public void test_player_b_win() throws InvalidInputException {
+        String winnerPlayer = tennisScore.getWinner("BABABB");
+        String expectedWinner = "Player B wins the game";
+        assertEquals(winnerPlayer, expectedWinner);
+    }
+
+    @Test
+    public void test_player_a_b_deuce_a_win() throws InvalidInputException {
+        String winnerPlayer = tennisScore.getWinner("ABABABAA");
+        String expectedWinner = "Player A wins the game";
+        assertEquals(winnerPlayer, expectedWinner);
+    }
+
+    @Test
+    public void test_player_a_b_deuce_b_win() throws InvalidInputException {
+        String winnerPlayer = tennisScore.getWinner("ABABABBABB");
+        String expectedWinner = "Player B wins the game";
+        assertEquals(winnerPlayer, expectedWinner);
     }
 }
