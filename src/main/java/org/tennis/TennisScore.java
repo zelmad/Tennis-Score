78d02ee6f18907a96f954ccc4baa064a63be6d3a
zelmadmohamed1994@gmail.com <Lhrto1994@>
2025-03-16 16:34:04 +0200

    Add initiating necessary files

--- src/main/java/org/tennis/TennisScore.java
+++ src/main/java/org/tennis/TennisScore.java
@@ -1,2 +1,13 @@
-package org.tennis;public class TennisScore {
+package org.tennis;
+
+public class TennisScore {
+
+    public String getWinner(String input) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        TennisScore tennisScore = new TennisScore();
+        System.out.println(tennisScore.getWinner("ABABABABAAAA"));
+    }
 }
--- src/test/java/TennisScoreTest.java
+++ src/test/java/TennisScoreTest.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class TennisScoreTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.tennis.TennisScore;
+
+public class TennisScoreTest {
+
+    private TennisScore tennisScore;
+
+    @BeforeEach
+    void setUp() {
+        tennisScore = new TennisScore();
+    }
 }
